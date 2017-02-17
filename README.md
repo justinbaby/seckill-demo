f1da039c2da065ea1f69f7afd406a468ca7673e3
justinbaby <2856416722@qq.com>
2017-02-17 21:13:58 +0800

    添加一下帮助

--- README.md
+++ README.md
@@ -12,12 +12,37 @@
  - Logback: 日志框架
  - JUnit: 单元测试
  - CDN: 内容分发服务器
- - Procedure:存储过程
+ - Procedure:数据库存储过程
  - Protostuff:Google开发的基于Java语言的序列化库
+
+## Reference
+
+详细开发教程可以参考[codingXiaxw前辈的开发文档](https://github.com/codingXiaxw/seckill),简直就是手把手教你做开发啊!!!
 
 ## Java高并发秒杀系统API
 
 本项目参考慕课网视频(版权方),并在此基础上进行了模块划分,功能添加，欢迎去官网观看!!!!
+
+## 安装部署
+ ####软件环境：
+  - IDEA
+  - MySQL
+  - JDK1.6或以上
+  - tomcat 8.0
+  - Redis
+  - Maven
+ ####硬件环境(最小配置)：
+   - CPU：1核
+   - 内存：1G
+ ####说明
+ - seckill-core：秒杀核心模块，部署tomcat启动
+ - seckill-api：秒杀api模块
+ - seckill-base：秒杀工具类模块
+ - seckill-web：秒杀页面模块
+ ####步骤
+ 1. 创建数据库，导入初始化脚本initDB.sql
+ 2. 修改系统数据库连接seckill-core/src/main/resources/jdbc.properties
+ 3. Tomcat运行
 
 ##FAQ
 - Q:为什么我的maven下载依赖jar包这么慢
--- docs/script/initDB.sql
+++ docs/script/initDB.sql
@@ -1,42 +1,106 @@
--- 数据库初始化脚本
+/*
 
--- 创建数据库
-CREATE DATABASE seckill;
--- 使用数据库
-USE seckill;
+Source Server         : root
+Source Server Version : 50538
+Source Host           : localhost:3306
+Source Database       : seckill
 
--- 创建秒杀库存表
-CREATE TABLE seckill(
-	`seckill_id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
-	`name` VARCHAR(120) NOT NULL COMMENT '商品名称',
-	`number` INT NOT NULL COMMENT '库存数量',
-	`start_time` TIMESTAMP NOT NULL COMMENT '秒杀开始时间',
-	`end_time` TIMESTAMP NOT NULL COMMENT '秒杀结束时间',
-	`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '秒杀创建时间',
-	PRIMARY KEY (`seckill_id`),
-	/*创建时间索引是为了以后时间查询的业务提供方便*/
-	KEY `idx_start_time` (`start_time`),
-	KEY `idx_end_time` (`end_time`),
-	KEY `idx_create_time` (`create_time`)
-)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表'
+Target Server Type    : MYSQL
+Target Server Version : 50538
+File Encoding         : 65001
 
--- 初始化数据
-INSERT INTO
-	seckill(name, number, start_time, end_time)
-VALUES
-	('1000元秒杀iphone6', 100, '2015-11-01 00:00:00', '2015-11-02 00:00:00'),
-	('500元秒杀ipad2', 200, '2015-11-01 00:00:00', '2015-11-02 00:00:00'),
-	('300元秒杀小米4', 300, '2015-11-01 00:00:00', '2015-11-02 00:00:00'),
-	('200元秒杀红米note', 400, '2015-11-01 00:00:00', '2015-11-02 00:00:00');
+Date: 2017-02-17 12:05:42
+*/
 
--- 秒杀成功明细表
--- 用户登录认证相关的信息
-CREATE TABLE success_killed(
-	`seckill_id` BIGINT NOT NULL COMMENT '商品库存id',
-	`user_phone` BIGINT NOT NULL COMMENT '用户手机号',
-	`state` TINYINT NOT NULL DEFAULT -1 COMMENT '状态信息：-1无效，0成功，1已付款，2已发货',
-	`create_time` TIMESTAMP NOT NULL COMMENT '创建时间',
-	PRIMARY KEY (`seckill_id`, `user_phone`),/*联合主键*/
-	KEY `idx_create_time` (`create_time`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀库存表'
+SET FOREIGN_KEY_CHECKS=0;
 
+-- ----------------------------
+-- Table structure for seckill
+-- ----------------------------
+DROP TABLE IF EXISTS `seckill`;
+CREATE TABLE `seckill` (
+  `seckill_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品库存ID',
+  `name` varchar(120) NOT NULL COMMENT '商品名称',
+  `number` int(11) NOT NULL COMMENT '库存数量',
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '秒杀开始时间',
+  `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '秒杀结束时间',
+  `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  PRIMARY KEY (`seckill_id`),
+  KEY `idx_start_time` (`start_time`),
+  KEY `idx_end_time` (`end_time`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1006 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表';
+
+-- ----------------------------
+-- Records of seckill
+-- ----------------------------
+INSERT INTO `seckill` VALUES ('1000', '100元秒杀诺基亚', '10000', '2017-02-17 12:02:05', '2019-12-21 00:00:00', '2016-12-21 00:00:00');
+INSERT INTO `seckill` VALUES ('1001', '5000元秒杀iphone7', '1700', '2017-02-17 11:47:36', '2016-04-11 00:00:00', '2016-04-11 00:00:00');
+INSERT INTO `seckill` VALUES ('1002', '1000元秒杀ipad1', '3500', '2017-02-17 11:47:41', '2016-05-12 00:00:00', '2016-05-12 00:00:00');
+INSERT INTO `seckill` VALUES ('1003', '1600元秒杀小米4', '1200', '2017-02-17 11:47:47', '2016-07-23 00:00:00', '2016-07-23 00:00:00');
+INSERT INTO `seckill` VALUES ('1004', '1400元秒杀魅族4', '1099', '2017-02-17 12:02:28', '2018-02-17 11:48:00', '2017-02-17 11:48:00');
+INSERT INTO `seckill` VALUES ('1005', '1400元秒杀小米3', '1100', '2017-02-17 11:48:23', '2016-01-21 00:00:00', '2016-01-21 00:00:00');
+
+-- ----------------------------
+-- Table structure for success_killed
+-- ----------------------------
+DROP TABLE IF EXISTS `success_killed`;
+CREATE TABLE `success_killed` (
+  `seckill_id` bigint(20) NOT NULL COMMENT '秒杀商品ID',
+  `user_phone` bigint(20) NOT NULL COMMENT '用户手机号',
+  `state` tinyint(4) NOT NULL DEFAULT '-1' COMMENT '状态标识:-1:无效 0:成功 1:已付款 2:已发货',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`seckill_id`,`user_phone`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
+
+-- ----------------------------
+-- Records of success_killed
+-- ----------------------------
+INSERT INTO `success_killed` VALUES ('1004', '15643645806', '-1', '2017-02-17 12:02:27');
+
+-- ----------------------------
+-- View structure for v1
+-- ----------------------------
+DROP VIEW IF EXISTS `v1`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `v1` AS SELECT *FROM seckill ;
+
+-- ----------------------------
+-- Procedure structure for execute_seckill
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `execute_seckill`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `execute_seckill`(IN v_seckill_id bigint, IN v_phone BIGINT,
+IN v_kill_time TIMESTAMP, OUT r_result INT)
+BEGIN
+		DECLARE insert_count INT DEFAULT 0;
+		START TRANSACTION;
+		INSERT ignore INTO success_killed (seckill_id, user_phone, create_time)
+		VALUES(v_seckill_id, v_phone, v_kill_time);
+		SELECT ROW_COUNT() INTO insert_count;
+		IF (insert_count = 0) THEN
+			ROLLBACK;
+			SET r_result = -1;
+		ELSEIF (insert_count < 0) THEN
+			ROLLBACK ;
+			SET r_result = -2;
+		ELSE
+			UPDATE seckill SET number = number - 1
+			WHERE seckill_id = v_seckill_id AND end_time > v_kill_time
+			AND start_time < v_kill_time AND number > 0;
+			SELECT ROW_COUNT() INTO insert_count;
+			IF (insert_count = 0) THEN
+				ROLLBACK;
+				SET r_result = 0;
+			ELSEIF (insert_count < 0) THEN
+				ROLLBACK;
+				SET r_result = -2;
+			ELSE
+				COMMIT;
+			SET r_result = 1;
+			END IF;
+		END IF;
+	END
+;;
+DELIMITER ;
+SET FOREIGN_KEY_CHECKS=1;
