a1c6420d48e2dea6161e8785dc2529b2dfa0f1b8
yanxianmin <2856416722@qq.com>
2022-11-27 18:02:50 +0800

    规范

--- seckill-service-provider/src/main/java/com/imooc/service/impl/App.java
+++ seckill-service-provider/src/main/java/com/imooc/service/impl/App.java
@@ -1,13 +1,19 @@
 package com.imooc.service.impl;
 
+import com.imooc.client.SeckillService;
+import com.imooc.entity.Seckill;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring/applicationContext.xml"});
         context.start();
+        SeckillService seckillService = context.getBean("seckillService",SeckillService.class);
+        List<Seckill> getSeckillList = seckillService.getSeckillList();
+        System.out.println(getSeckillList);
         System.out.println("press any key to exit.");
         System.in.read();
     }
